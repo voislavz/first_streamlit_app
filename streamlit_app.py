2efe4f4c4d57048c71dfc7f0e844430476688e12
voislavz <129840389+voislavz@users.noreply.github.com>
2023-04-04 16:28:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,3 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-import snowflake.connector
