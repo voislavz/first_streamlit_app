b01c864ce108c4574afac24b5bcd406dc99f1114
voislavz <129840389+voislavz@users.noreply.github.com>
2023-04-04 10:15:37 +0100

    Update streamlit_app.py
    pre-populating the pick list

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,4 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Advocado','Strawberries'])
